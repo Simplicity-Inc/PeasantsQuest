0c6446532e2c9e140dd3c9330ae18b7146e273f5
Nicole Long <nicole.long@sea.aie.edu>
2016-09-09 16:43:54 -0700

    daily task and money protoype

---
+++ Assets/Scripts/DailyTask.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DailyTask : MonoBehaviour {
+
+    private float currTime;
+
+    public Camera cam;
+
+    public GameObject player;
+
+    public MoneyManager moneyManager;
+
+    
+
+    // Use this for initialization
+    void Start() {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Vector3.Distance(player.transform.position, transform.position) <= 5f)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("mouse down");
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.name == this.name)
+                    {
+                        Debug.DrawLine(ray.origin, hit.point);
+                        Debug.Log("harvest begun");
+                        Harvest(5);
+                    }
+                }
+            }
+        }
+    }
+
+    
+    void Harvest(int finishValue)
+    {
+        Debug.Log("Harvesting complete");
+        moneyManager.CurrMoney += finishValue;
+         
+        Debug.Log(moneyManager.CurrMoney);
+    }
+}
---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyManager : MonoBehaviour {
+
+    private int currMoney;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int CurrMoney
+    {
+        get
+        {
+            return currMoney;
+        }
+        set
+        {
+            currMoney = value;
+        }
+    }
+
+}
